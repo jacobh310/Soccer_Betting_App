be33d887e7a5dbd6eda6e5bd30c852d4940a6b92
Jacob <jacob.hernandez0830@gmail.com>
2023-01-02 11:40:46 -0800

    fixed bug

--- Flask_app/main.py
+++ Flask_app/main.py
@@ -1,5 +1,6 @@
 from google.cloud import bigquery
 from flask import Flask, request, jsonify
+import os
 
 
 def get_fixtures():
@@ -32,4 +33,4 @@ def predictions():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
