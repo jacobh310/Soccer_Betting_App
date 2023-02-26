c617d3e1c4225fc3896ed55490b7d9a95ad68530
jacobh310 <74641471+jacobh310@users.noreply.github.com>
2023-02-26 10:43:37 -0800

    Changed text color

--- Frontend/pages/03_🚀_Model_Building.py
+++ Frontend/pages/03_🚀_Model_Building.py
@@ -27,14 +27,14 @@ hw_img = Image.open(BytesIO(hw_resp.content))
 ev_img = Image.open(BytesIO(ev_resp.content))
 
 col1.subheader('1st Strategy: Home over 50%')
-col1.markdown("""<p style="color:silver"> In this strategy, a $100 bet is taken when the model gives the home
+col1.markdown("""<p> In this strategy, a $100 bet is taken when the model gives the home
             team a greater than 50% of winning. If the home team won then I would use the betting odds to calculate the 
             Payout and add it to the current balance. If the home team lost then $100 is deducted from the current balance. The baseline profit/loss by betting the home
              team to win every time</p>""",  unsafe_allow_html=True)
 col1.image(hw_img, width=650)
 
 col2.subheader('2nd Strategy: Expected Value/Misprinted Odds')
-col2.markdown("""<p style="color:silver"> In this strategy, we convert the odds we get from the bookmaker into implied 
+col2.markdown("""<p> In this strategy, we convert the odds we get from the bookmaker into implied 
             probability. This implied probability is the probability the bookmaker set for a particular outcome. In this
              case, it's the home team. We take $100 bets only when our model probability is higher than the bookmakers. 
              If the home team wins, I use the odds to calculate the winnings and add it to the current balance. If the 
